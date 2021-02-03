54aa4f4367dee5c8b384aa15f2eb48dd28235664
bghanw <bghanw@bbcag.ch>
2021-02-03 15:52:14 +0100

    only btn2 will be expand when we  expand the window

--- src/main/java/HandVBox/HandVBox.java
+++ src/main/java/HandVBox/HandVBox.java
@@ -5,6 +5,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
 
@@ -18,6 +19,10 @@ public class HandVBox extends Application {
         Button btn1 = new Button("Button 1");
         Button btn2 = new Button("Button 2");
         Button btn3 = new Button("Button 3");
+        //HBox.setMargin(btn2, new Insets(0, 50, 0, 0)); // top, right, bottom, left
+        HBox.setHgrow(btn1, Priority.ALWAYS);
+        //btn1.setMaxWidth(200);
+        btn1.setMaxWidth(Double.MAX_VALUE);
         hbox.getChildren().addAll(btn1,btn2, btn3);
         Scene scene = new Scene(hbox);
         primaryStage.setScene(scene);
