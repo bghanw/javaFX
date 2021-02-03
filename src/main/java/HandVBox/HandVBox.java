c4c5c3167505cfb66d6775b53488b6d14f41e004
bghanw <bghanw@bbcag.ch>
2021-02-03 14:59:29 +0100

    Spaceing from around

--- src/main/java/HandVBox/HandVBox.java
+++ src/main/java/HandVBox/HandVBox.java
@@ -1,6 +1,7 @@
 package HandVBox;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
@@ -13,6 +14,7 @@ public class HandVBox extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         HBox hbox = new HBox(20);
+        hbox.setPadding(new Insets(20));
         Button btn1 = new Button("Button 1");
         Button btn2 = new Button("Button 2");
         Button btn3 = new Button("Button 3");
